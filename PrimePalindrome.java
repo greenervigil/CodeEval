9120c710acacc33d2d300918db076580ab3cf273
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-05-09 16:53:36 -0400

    worked on reverse methed in palindrome

--- PrimePalindrome.java
+++ PrimePalindrome.java
@@ -1,13 +1,16 @@
+import java.lang.*;
+
 class PrimePalindrome {
 
   private final static int MAX = 1000;
-  private static int div;
   private static int prime;
-  private static int num;
-  private static int rev;
+  private static int num = 2;
+  static int rev = 0;
 
   public static void main(String [] args){
-    for(num =2; num < MAX; num++){
+
+    // loop through 2 - 1000
+    for(; num < MAX; num++){
       if (isPrime(num) && isPalindrome(num)){
         prime = num;
       }
@@ -15,23 +18,12 @@ class PrimePalindrome {
     System.out.println(prime);
   }
 
-
-  //Test for Prime number
-  public static boolean isPrime(int num){
-      for(div = 2; div <= num/2; div++){
-        if (num % div == 0){
-          return false;
-        }
-      }
-      return true;
-  }
-
   //reverse the integer to test for possible palindrome
   // Modding by 10 extracts the rightmost digit
   //Multiply by 10 pushed left exposing a 0 to the right
   // Divide by 10 removes the rightmost digit
   public static int reverse(int number){
-    rev = 0;
+
     while (num != 0){
       int digit = num % 10;
       rev = rev * 10 + digit;
@@ -43,5 +35,17 @@ class PrimePalindrome {
   //return true or false if the palindrome condition is met
   public static boolean isPalindrome(int num){
     return num == reverse(num);
+    }
+    //Test for Prime number
+    public static boolean isPrime(int num){
+        if (num == 2) {
+          return true;
+        }
+        for(int div = 2; div <= num/2; div++){
+          if (num % div == 0){
+            return false;
+          }
+        }
+        return true;
     }
   }
