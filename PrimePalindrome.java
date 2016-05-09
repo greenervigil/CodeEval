4e10e3fb3500bb60e381e7373dc1961470aca74b
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-05-09 10:33:30 -0400

    Palindrome error clean up

--- PrimePalindrome.java
+++ PrimePalindrome.java
@@ -1,18 +1,21 @@
 class PrimePalindrome {
 
-  final static int max = 1000;
+  private final static int MAX = 1000;
   private static int div;
   private static int prime;
-  static int num = 2;
+  private static int num;
+  private static int rev;
 
   public static void main(String [] args){
-    for(; num < max; num++){
+    for(num =2; num < MAX; num++){
       if (isPrime(num) && isPalindrome(num)){
         prime = num;
       }
     }
     System.out.println(prime);
   }
+
+
   //Test for Prime number
   public static boolean isPrime(int num){
       for(div = 2; div <= num/2; div++){
@@ -23,7 +26,22 @@ class PrimePalindrome {
       return true;
   }
 
+  //reverse the integer to test for possible palindrome
+  // Modding by 10 extracts the rightmost digit
+  //Multiply by 10 pushed left exposing a 0 to the right
+  // Divide by 10 removes the rightmost digit
+  public static int reverse(int number){
+    rev = 0;
+    while (num != 0){
+      int digit = num % 10;
+      rev = rev * 10 + digit;
+      number = number / 10;
+    }
+    return rev;
+  }
+
+  //return true or false if the palindrome condition is met
   public static boolean isPalindrome(int num){
-    return true;
+    return num == reverse(num);
     }
   }
