c894f42cdfdc77c48b37af99893038dd9b7588dd
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-05-13 08:18:39 -0400

    Fibonacci Solution solved

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,14 +1,29 @@
+import java.io.*;
+
 class Fibonacci{
 
-  public static void main(String [] args){
-    int x;
-    System.out.println(getFib(x));
+  public static void main(String [] args) throws IOException{
+    //String numbers = "numbers.txt";
+    String line = null;
+
+    try {
+      FileReader fr = new FileReader(args[0]);
+      BufferedReader br = new BufferedReader(fr);
+      while ((line = br.readLine()) != null){
+        System.out.println(getFib(Integer.parseInt(line)));
+      }
+
+      br.close();
+    }
+    catch(FileNotFoundException e){
+      System.out.println("Could not open file.");
+    }
   }
 
   /**
   determines the Fibonacci sequence
   */
-  public int getFib(int n){
+  public static int getFib(int n){
     if (n <= 1){
       return n;
     }else{
