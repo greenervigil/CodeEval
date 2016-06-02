4872dfbc05af948e7a6c4460ed0dd0f3a5a03e0b
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-06-01 23:16:09 -0400

    Fib solution in Ruby

--- fib.rb
+++ fib.rb
@@ -1,19 +1,15 @@
-class Fib
-  attr_reader :line
+File.open(ARGV[0]).each_line do |line|
+   line.strip!
+   next if line.empty?
 
-  #check if file exists
-  puts "File exists!" if File.exist?(ARGV[0])
-  fileObj = File.open(ARGV[0]).each_line do |line|
-    while (line = fileObj.gets)
-      fib(line)
-    end
-  end
-  #find the f(n) solution
-  def fib(n)
-    if n <= 1
-      return n
-    else
-      return fib(n-1) + fib(n-2)
-    end
-  end
+   #find the f(n) solution
+   def fib(n)
+     if n <= 1
+       return n
+     else
+       return fib(n-1) + fib(n-2)
+     end
+   end
+puts fib(line.to_i)
+
 end
