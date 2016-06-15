978fbfa602b314c78ef2f939fdeb0a1975704b87
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-06-15 18:49:54 -0400

    Multiples solution in Java

--- incomplete/Multiples.java
+++ incomplete/Multiples.java
@@ -14,3 +14,36 @@ Print to stdout, the smallest multiple of n which is greater than or equal to x,
 16
 32
 */
+import java.io.*;
+
+public class Multiples{
+  public static void main(String [] args) throws IOException{
+    //get input from a file
+    String line = null;
+
+    try {
+      FileReader fr = new FileReader(args[0]);
+      BufferedReader br = new BufferedReader(fr);
+      while ((line = br.readLine()) != null){
+        String [] arr = line.split(",");
+        if (arr.length > 0){
+          int n = Integer.parseInt(arr[0]);
+          int m = Integer.parseInt(arr[1]);
+          System.out.println(lowestMultiple(n, m));
+        }
+      }
+      br.close();
+    }
+    catch(FileNotFoundException e){
+      System.out.println("Could not open file.");
+    }
+  }
+  public static int lowestMultiple(int cont, int start){
+    int sum = 0;
+    for (int i = 1; sum <= cont; i++){
+        sum = start * i;
+        //System.out.println(sum);
+    }
+    return sum;
+  }
+}
