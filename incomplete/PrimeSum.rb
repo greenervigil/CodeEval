4eb18726778397b8399365c8134e8e368210f272
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-07-09 19:48:58 -0400

    added documentation to solutions

--- incomplete/PrimeSum.rb
+++ incomplete/PrimeSum.rb
@@ -1,21 +1,26 @@
-sum = 0
-prime = 0
+class PrimeSum
+  sum = 0
+  prime = 0
 
-for n in 2..10000
-  if isPrime(n) then
-    sum += n
-    prime++
-  if prime == 1000 then
-    break
+  for n in 2..10000
+    if isPrime(n) then
+      sum += n
+      prime++
+    if prime == 1000 then
+      break
+  puts "#{sum}"
+  end
+  end
 end
-puts "#{sum}"
-
 def isPrime(number)
   if number == 2 then
     return true
+  end
   for x in 2..number/2
     if number % 2 == 0 then
       return false
+    end
   end
   return true
+end
 end
