2a7dfe9f17b8aca213afa276d866e57e798b8ff1
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-07-23 07:58:13 -0400

    working on Bit Position solution

--- incomplete/BitPos.java
+++ incomplete/BitPos.java
@@ -20,3 +20,42 @@ Print to stdout, 'true'(lowercase) if the bits are the same, else 'false'(lowerc
 true
 false
 */
+import java.io.*;
+import java.util.BitSet;
+
+public class BitPos {
+  public static void main(String [] args) throws IOException{
+
+    String line = null;
+    //look for file for input
+    try {
+      FileReader fr = new FileReader(args[0]);
+      BufferedReader br = new BufferedReader(fr);
+      while ((line = br.readLine()) != null){
+        //while there is a line with data send it to bitCompare method
+        System.out.println(bitCompare(line));
+      }
+      br.close();
+    }
+    catch(FileNotFoundException e){
+      System.out.println("Could not open file.");
+    }
+  }
+
+  public static Boolean bitCompare(String str) {
+    //separate the string into three parts; number, pos1, and pos2
+    String [] evalArray = str.split(",");
+    //convert number from int to bits
+    int number = Integer.parseInt(evalArray[0]);
+    BitSet b = new BitSet(number);
+    int pos1 = Integer.parseInt(evalArray[1]);
+    int pos2 = Integer.parseInt(evalArray[2]);
+
+    //compare the two positions for equality, return true if they are
+    // and false if they are not
+    if (b.get(pos1) == b.get(pos2)){
+      return true;
+    }
+    return false;
+  }
+}
