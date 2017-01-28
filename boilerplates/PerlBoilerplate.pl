eb6cd5a951d99dd57d986f642a76aaaba19af5c5
Daniel Vigil <daniel.julian.vigil@gmail.com>
2017-01-28 13:19:17 -0500

    added some boilerplate starters for JS Perl and Ruby

---
+++ boilerplates/PerlBoilerplate.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+open(INFILE, "<", $ARGV[0]) or die("Cannot open file $ARGV[0] for reading: $!");
+while(my $line = <INFILE>) {
+    next if($line =~ m/^\s$/);  # skip blank lines
+    $line =~ s/(^\s|\s*$)//g;   # remove leading and trailing spaces
+    ... do something with this line
+}
+close(INFILE);
