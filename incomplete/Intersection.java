63aafe42771db8ee006bb7927fb45bf8b6316d8d
Daniel Vigil <daniel.julian.vigil@gmail.com>
2022-08-28 00:20:37 -0400

    Update Intersection.java

--- incomplete/Intersection.java
+++ incomplete/Intersection.java
@@ -1,5 +1,6 @@
 /*
 You are given two sorted list of numbers (ascending order). The lists themselves are comma delimited and the two lists are semicolon delimited. Print out the intersection of these two sets.
+Print out the ascending order sorted intersection of the two lists, one per line. Print empty new line in case the lists have no intersection.
 
 INPUT SAMPLE:
 
@@ -8,9 +9,8 @@ File containing two lists of ascending order sorted integers, comma delimited, o
 20,21,22;45,46,47
 7,8,9;8,9,10,11,12
 OUTPUT SAMPLE:
-
-Print out the ascending order sorted intersection of the two lists, one per line. Print empty new line in case the lists have no intersection. E.g.
 4
 
 8,9
+
 */
