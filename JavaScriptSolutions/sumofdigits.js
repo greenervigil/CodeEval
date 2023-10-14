60f322fc6ff99cc041b44a718e5fa8034d553525
Daniel Vigil <daniel.julian.vigil@gmail.com>
2023-10-13 23:10:22 -0400

    Update sumofdigits.js
    update

--- JavaScriptSolutions/sumofdigits.js
+++ JavaScriptSolutions/sumofdigits.js
@@ -1,34 +1,36 @@
 /*Given a positive integer, find the sum of its constituent digits.
 
-//INPUT SAMPLE:
+INPUT SAMPLE:
 
-//The first argument will be a path to a filename containing positive integers, one per line. E.g.
+The first argument will be a path to a filename containing positive integers, one per line. E.g.
 
-//23
-//496
-//OUTPUT SAMPLE:
+23
+496
+OUTPUT SAMPLE:
 
-//Print to stdout, the sum of the numbers that make up the integer, one per line. E.g.
+Print to stdout, the sum of the numbers that make up the integer, one per line. E.g.
 
-//5
-//19
+5
+19
+
 */
-var fs  = require("fs");
+
+let fs  = require("fs");
 
 function summation(num){
-  var numbers = [];
-  var sum = 0;
-  for (var n = 0; n < num.length; n++) {
+  let numbers = [];
+  let sum = 0;
+  for (let n = 0; n < num.length; n++) {
     numbers[n] = parseInt(num.charAt(n));
   }
-  for (var i = 0; i < num.length; i++){
+  for (let i = 0; i < num.length; i++){
     sum += numbers[i];
   }
   console.log(sum);
 };
 
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
-    var sum = 0;
+    let sum = 0;
     if (line !== "") { // ignore empty lines
       summation(line);
     }
